7dff025b913eedd1fef2d7408625778ed2aae0e8
olgashulga-lev <rubyshulga@gmail.com>
2026-07-20 17:11:55 +0300

    Update shop.py

--- telegram/handlers/shop.py
+++ telegram/handlers/shop.py
@@ -13,25 +13,37 @@ async def cmd_shop(message: types.Message):
     if not player:
         await message.answer("Сначала нужно пройти регистрацию")
         return
-    items=api.get_items
-    shop_items = [i for i in items if i.type == 'shop']
+    items=api.get_items()
+    shop_items = [i for i in items if i.type == 'shop' or i.type == "permament"]
     if not shop_items:
         await message.answer('Магазин пуст')
         return
     text=f"<b>Магазин</b>\n Ваш баланс: {player.money}\n\n"
+
+    inventory=api.get_inventory(message.chat.id, message.from_user.id)
+    owned_permament_ids = [item.get('item_id') for item in inventory if item.get ('type') == 'permament']
+
     keyboard = InlineKeyboardMarkup(inline_keyboard=[])
     
     for item in shop_items:
         text += f"<b>{item.name}</b>\n"
         text += f"   {item.description}\n"
         text += f"   {item.price} монет\n\n"
-        
-        keyboard.inline_keyboard.append([
-            InlineKeyboardButton(
-                text=f"Купить {item.name} ({item.price})",
-                callback_data=f"buy_{item.id}"
-            )
-        ])
+        if item.type =='permament':
+            if item.id in owned_permament_ids:
+                text += "✅ Уже куплено\n"
+            else:
+                text+="⚠ Можно упить только один раз\n"
+        text+="\n"
+        if item.type == 'permament' and item.id in owned_permament_ids:
+            pass
+        else:
+            keyboard.inline_keyboard.append([
+                InlineKeyboardButton(
+                    text=f"Купить {item.name} ({item.price})",
+                    callback_data=f"buy_{item.id}"
+                )
+            ])
     await message.answer(text, reply_markup=keyboard)
 @router.callback_query(F.data.startwitch("buy_"))
 async def cmd_shop_buy(callback: types.CallbackQuery):
@@ -56,6 +68,12 @@ async def cmd_shop_buy(callback: types.CallbackQuery):
     if player.money<item.price:
         await callback.answer("Недостаточно средств")
         return
+    if item.type == 'permament':
+        inventory = api.get_inventory(callback.message.chat.id, callback.from_user.id)
+        for inv_item in inventory:
+            if inv_item.get('item_id')== item.id and inv_item.get('type') == 'permament':
+                await callback.answer(f"Вы уже купили {item.name}!")
+                return
     player.money-=item.price
     api.update_player(player)
     api.add_item_inventory(
@@ -65,6 +83,16 @@ async def cmd_shop_buy(callback: types.CallbackQuery):
         item_name=callback.item.name,
         item_type=callback.item.type
     )
-    await callback.answer(f"{item.name} приобретен")
+    if item.type == 'premament':
+        if item.id ==3:
+            player.damage+=15
+            api.update_player(player)
+            await callback.answer(f"Вы купили {item.name}! +15 к урону")
+        elif item.id==4:
+            player.hp=min(100,player.hp +20)
+            api.update_player(player)
+            await callback.answer(f"Вы купили {item.name}! +20 к HP")
+    else:
+        await callback.answer(f"{item.name} приобретен")
     text=f"<b>Магазин</b>\n Ваш баланс:{player.money}\n\n Куплено: {item.name}"
     await callback.message.edit_text(text)
