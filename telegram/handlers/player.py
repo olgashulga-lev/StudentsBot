f535205288bff3965f98ba4d483c05f8a19cf862
olgashulga-lev <rubyshulga@gmail.com>
2026-07-20 17:10:02 +0300

    Create player.py

---
+++ telegram/handlers/player.py
@@ -0,0 +1,44 @@
+import os
+from aiogram import Router, types
+from aiogram.filters import Command
+import api
+from .common_utils import get_player_or_none
+from aiogram.types import FSInputFile
+
+router = Router()
+
+@router.message(Command("avatar"))
+async def cmd_avatar(message: types.Message):
+    player=get_player_or_none(message)
+    if not player:
+        await message.answer("Сначала нужно пройти регистрацию")
+        return
+    lvl_player=player.level 
+    max2_level=player.max_level()
+    if lvl_player<max2_level:
+        need_exp_for_next_level=player.exp_next_level()
+        stroke_exp=f"Опыт: {player.exp}/{need_exp_for_next_level}"
+    else:
+        stroke_exp="Достигнут максимальный уровень"
+    text=f"""
+    <b>{player.name}</b>
+
+    Уровень: {player.level}/20
+    {stroke_exp}
+    HP:{player.hp}/100
+    Урон: {player.damage}
+    Удача: {int(player.luck*100)}
+    Деньги: {player.money}
+    """
+    # Проверяем существует ли файл
+    if os.path.exists(player.photo):
+        try:
+            photo = FSInputFile(player.photo)
+            await message.answer_photo(photo=photo,caption=text)
+        except Exception as e:
+           print(f"Ошибка при отправки фото:{e}")
+           await message.answer(text)
+            
+    else:
+        # Если файл не найден, отправляем только текст
+        await message.answer(text)
